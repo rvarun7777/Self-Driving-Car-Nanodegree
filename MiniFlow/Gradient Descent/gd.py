b0ddf15d40b113b2e86728a06eed3e6a459decc6
rvarun7777 <rvarun7777@gmail.com>
2016-12-05 13:38:37 +0100

    delete

--- MiniFlow/Gradient Descent/gd.py
+++
@@ -1,8 +0,0 @@
-def gradient_descent_update(x, gradx, learning_rate):
-    """
-    Performs a gradient descent update.
-    """
-    # TODO: Implement gradient descent.
-    x = x - learning_rate * gradx
-    # Return the new value for x
-    return x
